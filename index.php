beb9380788f3c372cca595159ffd88b3f595aeed
hoangthaison393 <81455267+hoangthaison393@users.noreply.github.com>
2021-04-13 10:01:56 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,25 +3,75 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>   
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>    
+    <title>Document</title> 
+
+    <!-- Bootstrap -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">  
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    
+    <!-- Google Fonts -->
     <link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,200,300,700,600' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Raleway:400,100' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="./styleneed.css">
-    <link rel="stylesheet" href="./style.css">
-    <link rel="stylesheet" href="./product.css">
-    <link rel="stylesheet" href="./option.css">
+    
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/owl.carousel.css"type="text/css"/>
+    <link rel="stylesheet" href="css/owl.theme.default.css"type="text/css"/>
+    <link rel="stylesheet" href="./styleneed.css" type="text/css"/>
+    <link rel="stylesheet" href="./style.css" type="text/css"/>
+    <link rel="stylesheet" href="./product.css" type="text/css"/>
+    <link rel="stylesheet" href="./option.css" type="text/css"/>
+     
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+
+    <!-- Jquery with Ajax -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+
+    <!-- Bootstrap JS form CDN -->
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+
+    <!-- jQuery easing -->
+    <script src="js/jquery.easing.1.3.min.js"></script>
+
+    <!-- jQuery sticky menu -->
+    <script src="js/owl.carousel.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.sticky.js"></script>
+
+    <!-- Main Script -->
+    <script src="js/main.js"></script>
+    
 </head>
 <body>
+<div class="header-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6">
+                <ul class="nav nav-pills">
+					<li><a href="#"><i class="fa fa-phone"></i> +84 944 623 114</a></li>
+					<li><a href="#"><i class="fa fa-envelope"></i> e-project-group-5@aptechlearning.edu.vn</a></li>
+				</ul>
+            </div>
+            <div class="col-sm-6">
+				<div class="social-icons pull-right">
+					<ul class="nav navbar-nav">
+						<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+						<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+						<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+						<li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+						<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+					</ul>
+				</div>
+			</div>
+        </div>
+    </div>
+</div>
+
     <div style="background-color: black;">
         <div class="container">
             <div class="row">
-                <div class="col-sm-3">
+                <div class="col-sm-2">
                     <div class="logo">
                         <h1><a href="./index.php">
                             <img src="./image/logofix6.png" style="height: 65px; ">
@@ -31,12 +81,11 @@
                 <div class="col-sm-4">
                     <div class="row border">
                         <div class="search-box">
-                            <div>
                                 <form id="form-search" role="form" method="GET" action="">
                                     <div>
                                         <div class="input-group ml-4" style="width: 100%">
                                             <input type="text" name="search" id="search" class="form-control"
-                                                   placeholder="findwhat">
+                                                   placeholder="Type here what you looking for...">
                                             <span class="input-group-btn">
                                                 <button id="btn-search" value="search" class="btn btn" style="background-color: rgb(254, 209, 1);">
                                                     <i class="fa fa-search"></i>
@@ -45,25 +94,21 @@
                                         </div>
                                     </div>
                                 </form>
-                            </div>
                         </div>  
                     </div>
                 </div>
-                <div class="col-sm-1">
-                    <div class="shopping-item">
-                        <a href="cart.php">Cart</a>
-                    </div>
-                </div>
-                <div class="col-sm-1">
-                    <div class="shopping-item">
-                        <a href="user-login.php">Login</a>
-                    </div>
-                </div>
-                <div class="col-sm-1">
-                    <div class="shopping-item">
-                        <a href="user-register">Register</a>
-                    </div>
-                </div>
+                <div class="col-md-6 clearfix">
+						<div class="shop-menu clearfix">
+							<ul class="nav navbar-nav">
+								<li><a href=""><i class="fa fa-user"></i> Account</a></li>
+								<li><a href=""><i class="fa fa-star"></i> Wishlist</a></li>
+								<li><a href="#"><i class="fa fa-crosshairs"></i> Checkout</a></li>
+								<li><a href="#"><i class="fa fa-shopping-cart"></i> Cart</a></li>
+								<li><a href="#"><i class="fa fa-lock"></i> Login</a></li>
+							</ul>
+						</div>
+					</div>
+                
         </div>
     </div>
     </div>
@@ -83,30 +128,100 @@
                 </div>  
             </div>
         </div>
+    </div> 
+
+<!--Slideshow -->
+<!-- Container for the image gallery -->
+<div class="container-fluid">
+  <!-- Full-width images with number text -->
+  <div class="mySlides" style="background-color: red">
+    <div class="numbertext">1 / 5</div>
+      <img src="image\slide\microwave1.jpg" style="width:100%">
+</div>
+
+  <div class="mySlides">
+    <div class="numbertext">2 / 5</div>
+      <img src="image\slide\bulbs.jpg" style="width:100%">
+  </div>
+
+  <div class="mySlides">
+    <div class="numbertext">3 / 5</div>
+      <img src="image\slide\fan.jpg" style="width:100%">
+  </div>
+
+  <div class="mySlides">
+    <div class="numbertext">4 / 5</div>
+      <img src="image\slide\fridge.png" style="width:100%">
+  </div>
+
+  <div class="mySlides">
+    <div class="numbertext">5 / 5</div>
+      <img src="image\slide\microwave.jpeg" style="width:100%">
+  </div>
+
+
+  <!-- Next and previous buttons -->
+  <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+  <a class="next" onclick="plusSlides(1)">&#10095;</a>
+
+  <!-- Image text -->
+  <div class="caption-container-fluid">
+    <p id="caption"></p>
+  </div>
+
+  <!-- Thumbnail images -->
+  <div class="row">
+    <div class="column">
+      <img class="demo cursor" src="image\slide\microwave1.jpg" style="width:100%" onclick="currentSlide(1)" alt="The Woods">
     </div>
-    <div style="background-color: black;" class="carousel-inner" role="listbox">
-        <div id="backgroundimg">
-            <div class="single-slide">
-                <div class="slide-bg slide-one"></div>
-                <div class="slide-text-wrapper">
-                    <div class="slide-text">
-                        <div class="container">
-                            <div class="row">
-                                <div class="col-md-6 col-md-offset-6">
-                                    <div class="slide-content">
-                                        <h2>We are awesome</h2>
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, dolorem, excepturi. Dolore aliquam quibusdam ut quae iure vero exercitationem ratione!</p>
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi ab molestiae minus reiciendis! Pariatur ab rerum, sapiente ex nostrum laudantium.</p>
-                                        <a class="readmore">Learn more</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+    <div class="column">
+      <img class="demo cursor" src="image\slide\bulbs.jpg" style="width:100%" onclick="currentSlide(2)" alt="Cinque Terre">
     </div>
+    <div class="column">
+      <img class="demo cursor" src="image\slide\fan.jpg" style="width:100%" onclick="currentSlide(3)" alt="Mountains and fjords">
+    </div>
+    <div class="column">
+      <img class="demo cursor" src="image\slide\fridge.png" style="width:100%" onclick="currentSlide(4)" alt="Northern Lights">
+    </div>
+    <div class="column">
+      <img class="demo cursor" src="image\slide\microwave.jpeg" style="width:100%" onclick="currentSlide(5)" alt="Nature and sunrise">
+    </div>
+  </div>
+</div>
+<script> //slide show javascript
+    var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+</script>
+<!--end slide show-->
+
     <p class="tittle" style="text-align: center; font-family: Garamond; margin-top: 15px;">TOP SELLING</p>
     <div class="productdiv">
         <div>
@@ -126,6 +241,33 @@
         </form>
         </div>
     </div>
+
+    <div class="slider-row">
+            <div class="owl-carousel owl-theme">
+                <div class="item"><img src="./image/slide/fan.jpg" alt="No image found"></div>
+                <div class="item"><img src="./image/slide/fridge.png" alt="No image found"></div>
+                <div class="item"><img src="./image/slide/microwave.jpeg" alt="No image found"></div>
+            </div>
+        </div>
+        
+        <script>
+            $('.owl-carousel').owlCarousel({
+                loop:true,
+                margin:10,
+                nav:true,
+                responsive:{
+                    0:{
+                        items:1
+                    },
+                    600:{
+                        items:3
+                    },
+                    1000:{
+                        items:5
+                    }
+                }
+            })
+        </script>
 <?php
 if(isset($_GET['productid']))
 {
@@ -135,5 +277,5 @@ function redirect($url){
 echo '<script>window.location="'.$url.'"</script>';
 }
 ?>
-</body>
+    </body>
 </html>
